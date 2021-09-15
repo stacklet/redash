22c68de1a26725ff8f0dc7819e899b5d274bf63a
Wayne Witzel III <wayne@stacklet.io>
2021-09-15 18:36:37 -0400

    sa url not a string

--- redash/cli/database.py
+++ redash/cli/database.py
@@ -31,11 +31,8 @@ def _wait_for_db_connection(db):
 def is_db_empty():
     from redash.models import db
 
-    extant_tables = set(
-        sqlalchemy.inspect(
-            db.create_engine(settings.SQLALCHEMY_DATABASE_URI, {})
-        ).get_table_names()
-    )
+    url = sqlalchemy.engine.url.make_url(settings.SQLALCHEMY_DATABASE_URI)
+    extant_tables = set(sqlalchemy.inspect(db.create_engine(url, {})).get_table_names())
     redash_tables = set(db.metadata.tables)
     return len(redash_tables.intersection(extant_tables)) == 0
 
