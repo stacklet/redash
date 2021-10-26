0504971ea7cd270ced1d2208ea3f0728d3a8fd93
Wayne Witzel III <wayne@stacklet.io>
2021-10-26 08:33:53 -0400

    feat: add cors so embedded dashboards can work cross domain (#14)

--- redash/handlers/dashboards.py
+++ redash/handlers/dashboards.py
@@ -9,8 +9,8 @@ from redash.handlers.base import (
     filter_by_tags,
     get_object_or_404,
     paginate,
+    order_results as _order_results,
 )
-from redash.handlers.base import order_results as _order_results
 from redash.permissions import (
     can_modify,
     require_admin_or_owner,
