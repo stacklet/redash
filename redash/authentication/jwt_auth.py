7d8c8cd98d82f09d9a3a50709878a75df9ac9659
Samuel Cozannet <samuel.cozannet@madeden.com>
2022-07-14 09:57:52 +0200

    fix: compat with Athena queries by locking PyAthena to a max version

--- redash/authentication/jwt_auth.py
+++ redash/authentication/jwt_auth.py
@@ -1,45 +1,19 @@
-import json
 import logging
-
 import jwt
 import requests
+import simplejson
 from jwt.exceptions import (
     PyJWTError,
+    ImmatureSignatureError,
+    InvalidKeyError,
+    InvalidSignatureError,
     InvalidTokenError,
-    MissingRequiredClaimError,
+    ExpiredSignatureError,
 )
 
 from redash.settings.organization import settings as org_settings
 
 logger = logging.getLogger("jwt_auth")
-
-FILE_SCHEME_PREFIX = "file://"
-
-
-def get_public_key_from_file(url):
-    file_path = url[len(FILE_SCHEME_PREFIX) :]
-    with open(file_path) as key_file:
-        key_str = key_file.read()
-
-    get_public_keys.key_cache[url] = [key_str]
-    return key_str
-
-
-def get_public_key_from_net(url):
-    r = requests.get(url)
-    r.raise_for_status()
-    data = r.json()
-    if "keys" in data:
-        public_keys = []
-        for key_dict in data["keys"]:
-            public_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(key_dict))
-            public_keys.append(public_key)
-
-        get_public_keys.key_cache[url] = public_keys
-        return public_keys
-    else:
-        get_public_keys.key_cache[url] = data
-        return data
 
 
 def get_public_keys(url):
@@ -48,18 +22,46 @@ def get_public_keys(url):
         List of RSA public keys usable by PyJWT.
     """
     key_cache = get_public_keys.key_cache
-    keys = {}
     if url in key_cache:
-        keys = key_cache[url]
+        return key_cache[url]
     else:
-        if url.startswith(FILE_SCHEME_PREFIX):
-            keys = [get_public_key_from_file(url)]
+        r = requests.get(url)
+        r.raise_for_status()
+        data = r.json()
+        if "keys" in data:
+            public_keys = []
+            for key_dict in data["keys"]:
+                public_key = jwt.algorithms.RSAAlgorithm.from_jwk(
+                    simplejson.dumps(key_dict)
+                )
+                public_keys.append(public_key)
+
+            get_public_keys.key_cache[url] = public_keys
+            return public_keys
         else:
-            keys = get_public_key_from_net(url)
-    return keys
+            get_public_keys.key_cache[url] = data
+            return data
 
 
 get_public_keys.key_cache = {}
+
+
+def find_identity_in_payload(payload):
+    if "email" in payload:
+        return payload["email"]
+    if "identities" in payload:
+        for identity in payload["identities"]:
+            if "email" in identity:
+                return identity["email"]
+            elif "userId" in identity:
+                return identity["userId"]
+            elif "nameId" in identity:
+                return identity["nameId"]
+    elif "username" in payload:
+        return payload["username"]
+    elif "cognito:username" in payload:
+        return payload["cognito:username"]
+    return None
 
 
 def verify_jwt_token(
@@ -73,32 +75,62 @@ def verify_jwt_token(
     try:
         key_id = jwt.get_unverified_header(jwt_token).get("kid", "")
     except PyJWTError as e:
-        logger.info("Ignoring invalid JWT token: %s", e)
-        return None, False
+        logger.info("Rejecting invalid JWT token: %s", e)
+        raise
 
     if key_id and isinstance(keys, dict):
         keys = [keys.get(key_id)]
 
-    valid_token = False
     payload = None
+    identity = None
+    valid_token = False
+    any_key_valid = False
     for i, key in enumerate(keys):
         try:
             # decode returns the claims which has the email if you need it
-            payload = jwt.decode(jwt_token, key=key, audience=expected_audience, algorithms=algorithms)
+            payload = jwt.decode(
+                jwt_token, key=key, audience=expected_audience, algorithms=algorithms
+            )
+            any_key_valid = True
             issuer = payload["iss"]
             if issuer != expected_issuer:
-                raise InvalidTokenError("Wrong issuer: {}".format(issuer))
+                raise InvalidTokenError('Token has incorrect "issuer"')
             client_id = payload.get("client_id")
             if expected_client_id and expected_client_id != client_id:
-                raise InvalidTokenError("Wrong client_id: {}".format(client_id))
-            user_claim = org_settings["auth_jwt_auth_user_claim"]
-            if not payload.get(user_claim):
-                raise MissingRequiredClaimError(user_claim)
+                raise InvalidTokenError('Token has incorrect "client_id"')
+            identity = find_identity_in_payload(payload)
+            if not identity:
+                raise InvalidTokenError(
+                    "Unable to determine identity (missing email, username, or other identifier)"
+                )
             valid_token = True
             break
-        except PyJWTError as e:
+        except (InvalidKeyError, InvalidSignatureError) as e:
             logger.info("Rejecting JWT token for key %d: %s", i, e)
+            # Key servers can host multiple keys, only one of which would
+            # actually be used for a given token. So if the check failed
+            # due only to an issue with this key, we should just move on
+            # to the next one.
+            continue
+        except (ImmatureSignatureError, ExpiredSignatureError) as e:
+            logger.info("Rejecting JWT token: %s", e)
+            # The key checked out, but the token was outside of the time-window
+            # that it should be valid for. This is not an error but means they'll
+            # need to log in again.
+            any_key_valid = True
+            continue
+        except InvalidTokenError as e:
+            logger.error("Rejecting invalid JWT token: %s", e)
+            # Any other issue with the token means it has a fundamental issue so
+            # if we send them to the login page it could cause a redirect loop.
+            raise
         except Exception as e:
             logger.exception("Error processing JWT token: %s", e)
-            break
-    return payload, valid_token
+            raise InvalidTokenError("Error processing token") from e
+    if not any_key_valid:
+        logger.error("No valid keys for token")
+        # If none of the keys from the key server are valid, it's a auth server
+        # misconfiguration and sending them to the login page would definitely
+        # cause a redirect loop.
+        raise InvalidTokenError("No valid keys for token")
+    return payload, identity, valid_token
