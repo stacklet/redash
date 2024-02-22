58551faa5561da83175807e0f73febcc8d2e6deb
thisisshi <theli@outlook.com>
2024-02-22 15:45:59 -0800

    merge migration heads

---
+++ migrations/versions/4a777fe87033_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4a777fe87033
+Revises: 4afa4a1dd310, 7205816877ec
+Create Date: 2024-02-22 15:45:19.527592
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4a777fe87033'
+down_revision = ('4afa4a1dd310', '7205816877ec')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
