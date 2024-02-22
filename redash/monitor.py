5ed18d2774bb6bdb1f68d4df7817d09a6aa37ea7
thisisshi <theli@outlook.com>
2024-02-22 15:00:42 -0800

    missed merge

--- redash/monitor.py
+++ redash/monitor.py
@@ -1,17 +1,11 @@
 from funcy import flatten
-<<<<<<< HEAD
-from sqlalchemy import union_all
-from redash import redis_connection, rq_redis_connection, __version__, settings
-from redash.models import db, DataSource, Query, QueryResult, Dashboard, Widget
-from redash.utils import json_loads, get_schema
-=======
->>>>>>> upstream/master
 from rq import Queue, Worker
 from rq.job import Job
 from rq.registry import StartedJobRegistry
 
 from redash import __version__, redis_connection, rq_redis_connection, settings
 from redash.models import Dashboard, Query, QueryResult, Widget, db
+from redash.utils import get_schema
 
 
 def get_redis_status():
