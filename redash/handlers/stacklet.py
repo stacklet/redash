6839e72add7cc8c37af6be5b1d5b3f4cbdd791fe
kit <kit@stacklet.io>
2026-01-27 10:35:33 +1300

    add functool caching decorator

--- redash/handlers/stacklet.py
+++ redash/handlers/stacklet.py
@@ -1,3 +1,4 @@
+import functools
 from flask_login import login_required
 from redash.handlers import routes
 from redash.handlers.base import json_response
@@ -7,6 +8,7 @@ from redash.stacklet.config import get_application_config
 @routes.route("/stacklet/config", methods=["GET"])
 @talisman(force_https=False)
 @login_required
+@functools.cache
 def stacklet_config():
     """Endpoint to expose application configuration from AWS SSM Parameter Store."""
     config = get_application_config()
