49fd3ad7f5e982fdae468776a7fa31f8d71743d2
kit <kit@stacklet.io>
2026-01-27 09:49:03 +1300

    use @login_required decorator

--- redash/handlers/stacklet.py
+++ redash/handlers/stacklet.py
@@ -1,11 +1,12 @@
+from flask_login import login_required
 from redash.handlers import routes
 from redash.handlers.base import json_response
 from redash.security import talisman
 from redash.stacklet.config import get_application_config
 
-
 @routes.route("/stacklet/config", methods=["GET"])
 @talisman(force_https=False)
+@login_required
 def stacklet_config():
     """Endpoint to expose application configuration from AWS SSM Parameter Store."""
     config = get_application_config()
