6ccc7ce874869ac0e14af7f398bc78e6d113adee
James Troup <james@stacklet.io>
2026-03-14 19:42:37 +0000

    chore: chain cy.get assertions + clicks to avoid detached-DOM flakiness

--- client/cypress/integration/visualizations/table/table_spec.js
+++ client/cypress/integration/visualizations/table/table_spec.js
@@ -43,10 +43,8 @@ describe("Table", () => {
     const { query, config } = AllCellTypes;
     prepareVisualization(query, "TABLE", "All cell types", config).then(() => {
       // expand JSON cell
-      cy.get(".jvi-item.jvi-root .jvi-toggle").should("be.visible");
-      cy.get(".jvi-item.jvi-root .jvi-toggle").click();
-      cy.get(".jvi-item.jvi-root .jvi-item .jvi-toggle").should("be.visible");
-      cy.get(".jvi-item.jvi-root .jvi-item .jvi-toggle").click({ multiple: true });
+      cy.get(".jvi-item.jvi-root .jvi-toggle").should("be.visible").click();
+      cy.get(".jvi-item.jvi-root .jvi-item .jvi-toggle").should("be.visible").click({ multiple: true });
 
       cy.percySnapshot("Visualizations - Table (All cell types)", { widths: [viewportWidth] });
     });
